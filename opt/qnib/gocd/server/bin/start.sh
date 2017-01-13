c9da909aaba1ebc35d59f03e6dfac825215581f9
Christian Kniep <ckniep@gaikai.com>
2017-01-13 13:12:22 +0100

    refine dockerfile and add restore entrypoint

--- opt/qnib/gocd/server/bin/start.sh
+++ opt/qnib/gocd/server/bin/start.sh
@@ -1,9 +1,4 @@
-#!/usr/local/bin/dumb-init /bin/bash
-
-## Restore if needed
-if [ $(ls /opt/go-server/artifacts/serverBackups/ |grep backup_ |wc -l) -ne 0 ];then
-    /opt/qnib/gocd/server/bin/restore.sh
-fi
+#!/bin/bash
 
 if [ "X${GOCD_SERVER_CLEAN_WORKSPACE}" == "Xtrue" ];then
     rm -f /opt/go-server/config/*
@@ -13,6 +8,4 @@ if [ ! -f /opt/go-server/config/cruise-config.xml ];then
     consul-template -once -template "/etc/consul-templates/gocd/server/cruise-config.xml.ctmpl:/opt/go-server/config/cruise-config.xml"
 fi
 
-/opt/go-server/server.sh 2>&1 1>/var/log/go-server.log
-echo $$ > /opt/go-server/go-server.pid
-tail -f /var/log/go-server.log
+/opt/go-server/server.sh 
